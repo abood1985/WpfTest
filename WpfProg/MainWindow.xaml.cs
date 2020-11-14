667742c18ddaeff4b60fc705f5373df706dafd6f
Abdallah Sbaih <abdallah.m.s.1985@gmail.com>
2020-11-14 14:19:54 +0200

    first commit

---
+++ WpfProg/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace WpfProg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PosTutEntities db = new PosTutEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            System.Windows.Data.CollectionViewSource productViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("productViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // productViewSource.Source = [generic data source]
+
+            //resultcombo_SelectionChanged(this.resultcombo, null);
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+              
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($" This Description is : {this.Desc.Text} ");
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.ch1.IsChecked = this.ch2.IsChecked = this.ch3.IsChecked = this.ch4.IsChecked = this.ch5.IsChecked =
+            this.ch11.IsChecked = this.ch22.IsChecked = this.ch33.IsChecked = this.ch44.IsChecked = this.ch55.IsChecked = false;
+
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.Desc.Text += ((CheckBox)sender).Content;
+            this.Desc.Text = this.Desc.Text + "/";
+        }
+              
+
+        private void resultcombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.resltname == null)
+                return;
+
+            var combo = (ComboBox)sender;
+            var valuee = (ComboBoxItem)combo.SelectedValue;
+
+            this.resltname.Text = (string)valuee.Content;
+
+        }
+    }
+}
---
+++ WpfProg/TreeUI.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfProg
+{
+    /// <summary>
+    /// Interaction logic for TreeUI.xaml
+    /// </summary>
+    public partial class TreeUI : Window
+    {
+        public TreeUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+                item.Header = drive;
+                item.Tag = drive;
+                item.Items.Add(null);
+                item.Expanded += Folder_Expanded;
+
+                FolderView.Items.Add(item);
+            }
+
+           
+
+
+
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            var item = (TreeViewItem)sender;
+
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            item.Items.Clear();
+
+            var fullpath = (string)item.Tag;
+
+            var directories = new List<string>();
+
+            try
+            {
+                var dirs = Directory.GetDirectories(fullpath);
+
+                if (dirs.Length > 0)
+                    directories.AddRange(dirs);
+
+            }
+            
+             catch { }
+
+            directories.ForEach(directoryPath =>
+           {
+               var subItem = new TreeViewItem();
+               {
+                   item.Header = GetFileFolderName(directoryPath);
+                   Tag = directoryPath;
+
+               };
+
+               subItem.Items.Add(null);
+               subItem.Expanded += Folder_Expanded;
+               item.Items.Add(subItem);
+           });
+      
+    }
+
+    public static string GetFileFolderName(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+            return string.Empty;
+
+        var normalizedpath = path.Replace('/', '\\');
+        var lastindex = normalizedpath.LastIndexOf('\\');
+
+        if (lastindex <= 0)
+            return path;
+
+        return path.Substring(lastindex + 1);
+
+    }
+}
+}
